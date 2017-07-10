b261635a94da8fd4068fedda42e1a76b8156c875
Abinash Meher <abinashdakshana999@gmail.com>
2017-07-11 01:05:32 +0530

    Logic for __init__ of Simulation

--- distalg/simulation.py
+++ distalg/simulation.py
@@ -1,25 +1,31 @@
 import networkx as nx
-from copy import deepcopy
 from process import Process
 from channel import Channel
-from action import Action
 
 
 class Simulation:
-    def __init__(self, embedding_graph=nx.Graph):
-        if isinstance(embedding_graph, type):
-            embedding_graph = embedding_graph()  # readable and dry alternative for object as default arg
+    def __init__(self, embedding_graph=None, process_type=Process, channel_type=Channel):
 
-        self.graph = nx.Graph()
-        process_map ={}
-        for node in embedding_graph.nodes_iter():
-            if node not in process_map:
-                process_map[node] = Process()
-                self.graph.add_node(process_map[node])
+        self.graph = nx.freeze(embedding_graph)
 
-        for edge in embedding_graph.edges_iter():
-            u, v = edge
-            self.graph.add_edge(process_map[u], process_map[v], object=Channel())
+        self.process_map = {}
+        self.node_map = {}
+        self.channel_map = {}
+
+        for n, neighbors_dict in self.graph.adjacency_iter():
+            if n not in self.process_map:
+                self.process_map[n] = process_type()
+                self.node_map[self.process_map[n].id] = n
+
+            for neighbor, edge_attr in neighbors_dict.items():
+                if neighbor not in self.process_map:
+                    self.process_map[neighbor] = process_type()
+                    self.node_map[self.process_map[neighbor].id] = neighbor
+
+                channel = channel_type()
+                self.channel_map[(n, neighbor)] = channel
+                self.process_map[n].out_channel.append(channel)
+                self.process_map[neighbor].in_channel.append(channel)
 
     def run(self):
         # starts the simulation
