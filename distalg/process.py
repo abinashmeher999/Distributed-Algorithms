3e394135a8db18ae91859093da7b26ea52abb5ff
Abinash Meher <abinashdakshana999@gmail.com>
2017-07-10 23:29:33 +0530

    Add shortuuid as id for Process

--- distalg/process.py
+++ distalg/process.py
@@ -1,7 +1,21 @@
-class Process:
+import shortuuid
+
+
+class Process(object):
     def __init__(self):
+        self._id = shortuuid.uuid()
         self.in_channels = []
         self.out_channels = []
 
     async def onreceive(self, msg):
         pass
+
+    @property
+    def id(self):
+        return self._id
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+    def __hash__(self):
+        return hash(self.id)
