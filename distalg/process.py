b348af877b7bb4db8ba9e1d6b7eb181d8b4b5c66
Abinash Meher <abinashdakshana999@gmail.com>
2017-07-23 23:00:06 +0530

    Draft implementation for subroutine support

--- distalg/process.py
+++ distalg/process.py
@@ -1,4 +1,6 @@
 import asyncio
+from collections import OrderedDict
+
 import shortuuid
 from multipledispatch import dispatch
 from distalg.message import Message
@@ -26,6 +28,7 @@ class Process(object):
     def _receive_msgs_creator(self):
         def _receive_msgs():
             return Process.ReceiverAsyncIterable(self)
+
         return _receive_msgs
 
     def __init__(self, pid=None):
@@ -34,6 +37,9 @@ class Process(object):
         self.out_channels = []
         self.incoming_msgs = asyncio.Queue()
         self.receive_msgs = self._receive_msgs_creator()
+        self.state = {}
+        self._parent_state = None
+        self.subroutines = OrderedDict()
 
     async def receive_a_msg(self):
         """
@@ -56,6 +62,21 @@ class Process(object):
     @property
     def id(self):
         return str(self._id)
+
+    @property
+    def parent_state(self):
+        return self._parent_state
+
+    def add_subroutine(self, name, process_instance):
+        """
+        :param name: The name of the subroutine eg: "echo1", "echo2",...
+        :param process_instance: An instance of the Process class or its subclass with method `run`
+        :return: None
+        """
+        process_instance.in_channels = self.in_channels
+        process_instance.out_channels = self.out_channels
+        process_instance._parent_state = self.state
+        self.subroutines[name] = process_instance
 
     def __eq__(self, other):
         return self.id == other.id
